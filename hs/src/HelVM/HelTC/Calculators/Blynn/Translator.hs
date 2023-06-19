3c8f41699d4e7a80182e1019d8bf9fe68963369a
Kamil Adam <kamil.adam.zabinski@gmail.com>
2023-06-19 21:41:22 +0200

    Blynn lambda

--- hs/src/HelVM/HelTC/Calculators/Blynn/Translator.hs
+++ hs/src/HelVM/HelTC/Calculators/Blynn/Translator.hs
@@ -5,8 +5,10 @@ import           HelVM.HelTC.Calculators.Blynn.Lambda
 import qualified HelVM.HelTC.Calculators.LC.Lambda    as Hel
 
 translate :: Hel.Lambda -> LC
-translate Hel.S = Other S
-translate Hel.K = Other K
-translate Hel.I = Other I
-translate Hel.Var n = 
-
+translate  Hel.S        = Other S
+translate  Hel.K        = Other K
+translate  Hel.I        = Other I
+translate (Hel.Var n)   = Var $ toString n
+translate (Hel.Abs n f) = Lam (toString n) (translate f)
+translate (Hel.App f g) = translate f :@ translate g
+translate _             = error "error in translate"

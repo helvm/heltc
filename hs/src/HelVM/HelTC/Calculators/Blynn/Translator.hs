1db82e53031688d9b1caadafd77df9a4bb8771c6
Kamil Adam <kamil.adam.zabinski@gmail.com>
2023-06-19 21:35:42 +0200

    Blynn lambda

--- hs/src/HelVM/HelTC/Calculators/Blynn/Translator.hs
+++ hs/src/HelVM/HelTC/Calculators/Blynn/Translator.hs
@@ -1,7 +1,12 @@
 module HelVM.HelTC.Calculators.Blynn.Translator where
 
-import qualified HelVM.HelTC.Calculators.Blynn.Lambda as Blynn
+import           HelVM.HelTC.Calculators.Blynn.Lambda
 
 import qualified HelVM.HelTC.Calculators.LC.Lambda    as Hel
 
-translate :: Hel.Lambda -> Blynn.LC
+translate :: Hel.Lambda -> LC
+translate Hel.S = Other S
+translate Hel.K = Other K
+translate Hel.I = Other I
+translate Hel.Var n = 
+
